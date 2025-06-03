1b742ad0f3832193548ee3545a5faa874965b31f
sm222 <soleil.maraska@gmail.com>
2025-06-02 23:40:10 -0400

    almost done with makefile bash scrips

--- linux/buildMakefile.sh
+++ linux/buildMakefile.sh
@@ -10,16 +10,30 @@ CYN=\\x1B[36m
 WHT=\\x1B[37m
 RESET=\\x1B[0m
 
+################################
+#
+#          Setting
+#
+################################
+
+raylibPath='../Engine/include/raylib/src/libraylib.a'
+
 buildTime=$(bash -c date)
 platform=$(bash -c uname -os)
 madeby='@sm222'
 
 sysHeder=(
-  '# made by'
+  '#####################################'
+  '#	made by'
   "#		$madeby"
   "#		$buildTime"
   "#		$platform"
-  '# - - - - - -  -'
+  '#####################################'
+  ''
+  '########################################'
+  "#	Don't edit that file by hands, call    $0"
+  '#		and edit the variable in varAndFiles'
+  '########################################'
 )
 
 i_sysHeader=${#sysHeder[@]}
@@ -34,28 +48,18 @@ function printSysHeader() {
   done
 }
 
-#
-winHeader=(
-  ''
-  ''
-  ''
-  '# WINDOS MAKEFILE '
-  'NAME			= IceFactory'
-  'NAME_X		= .exe'
-  ''
-  '# - - - -'
-  ''
-  'RAYLIB		=	../Engine/include/raylib/src/libraylib.a'
-  'ENGINE_FLAGS		=		$(RAYLIB) -lgdi32 -lwinmm'
-  'ENGINE_SETTING	=		-D 'HOT_RELOAD=1' -Wno-missing-field-initializers'
-  'DEBUG						=		-g'
-)
 
-normalHeader=(
+################################
+#
+#          C/C++
+#
+################################
+
+compiler=(
   ''
   ''
   '#  compiler'
-  'CC				=			cc'
+  'CC				=		cc'
   'CXX			=			g++'
   ''
   ''
@@ -64,31 +68,247 @@ normalHeader=(
   'CFLAGS		=			$(DEBUG) $(ENGINE_SETTING) -Wall -Werror -Wextra'
   'CXXFLAGS	=			$(DEBUG) $(ENGINE_SETTING) -Wall -Werror -Wextra'
   ''
-
 )
+
+i_compiler=${#compiler[@]}
+j_compiler=0
+
+function drawCompiler() {
+  echo -n
+  while [ $j_compiler -lt $i_compiler ]
+  do
+    echo "${compiler[$j_compiler]}" >> $1
+    j_compiler=$((j_compiler + 1))
+  done
+}
+
+
+################################
+#
+#          Engine
+#
+################################
 
 varAndFiles=(
-  ""
+  '#	engine root'
+  'ENGINE		=			../Engine/'
+  ''
+  '#	Base'
+  ''
+  'BASE				=		$(ENGINE)Base/Base.cpp'
+  ''
+  ''
+  'CAMERA			=		\'
+  '$(ENGINE)Camera/BaseCamera.cpp \'
+  '$(ENGINE)Camera/Base2DCamera.cpp'
+  ''
+  '#Group'
+  'GROUPS			=		$(ENGINE)Groups/BeseGroups.cpp'
+  ''
+  ''
+  '#'
+  ''
+  '# -- IMPORT -- #'
+  ''
+  'IMPORT			=		$(ENGINE)Import/'
+  ''
+  'I_MODEL			=		$(IMPORT)Model/ModelManager.cpp'
+  ''
+  'I_AUDIO			=		$(IMPORT)Audio/AudioManager.cpp'
+  ''
+  'I_TEXTURE2D	=		$(IMPORT)Texture2D/Texture2DManager.cpp'
+  ''
+  '#  --  --  --  #'
+  ''
+  ''
+  'OBJECT			=	\'
+  '$(ENGINE)Object/DevCube.cpp		\'
+  '$(ENGINE)Object/MeshObject.cpp\'
+  '$(ENGINE)Object/Object3D.cpp	\'
+  '$(ENGINE)Object/Object2D.cpp	\'
+  '$(ENGINE)Object/Object.cpp		\'
+  ''
+  ''
+  'RENDER			=	$(ENGINE)Render/Render.cpp'
+  ''
+  'DRAW2D			=	$(ENGINE)Draw2D/DrawTexture2D.cpp'
+  ''
+  'ROOM				=	$(ENGINE)Room/Room.cpp'
+  ''
+  ''
+  'UI					=	\'
+  '$(ENGINE)Ui/UiBaseBlock.cpp		\'
+  '$(ENGINE)Ui/UiBaseTextBox.cpp	\'
+  '$(ENGINE)Ui/UiRenderZone.cpp	\'
 )
 
 
-linuxFilename=makeFileLinux
+i_varAndFiles=${#varAndFiles[@]}
+j_varAndFiles=0
+
+function printFiles() {
+  echo -n
+  while [ $j_varAndFiles -lt $i_varAndFiles ]
+  do
+    echo "${varAndFiles[$j_varAndFiles]}" >> $1
+    j_varAndFiles=$((j_varAndFiles + 1))
+  done
+}
+
+################################
+#
+#          Game
+#
+################################
+
+gameFiles=(
+  ''
+  '# Game root'
+  'GAME			=		../Game/'
+  ''
+  'GAMEFILE	= $(GAME)main.cpp'
+  ''
+  ''
+)
+
+i_gameFiles=${#gameFiles[@]}
+j_gameFiles=0
+
+function printGameFiles() {
+  echo -n
+  while [ $j_gameFiles -lt $i_gameFiles ]
+  do
+    echo "${gameFiles[$j_gameFiles]}" >> $1
+    j_gameFiles=$((j_gameFiles + 1))
+  done
+}
+
+
+################################
+#
+#          compile rule
+#
+################################
+
+compileRule=(
+  ''
+  ''
+  'C_OBJS		=	$(C_SRCS:.c=.o)'
+  'CPP_OBJS	=	$(CPP_SRCS:.cpp=.o)'
+  ''
+  ''
+  'all: buildraylib $(NAME)'
+  '	@mv -v $(NAME)$(NAME_X) ..'
+  ''
+  '$(NAME): Engine'
+  '	@echo | $(NAME)$(NAME_X) |'
+  ''
+  'Engine: $(CXX)'
+  '	@echo'
+  ''
+  '$(CXX): $(CPP_OBJS)'
+  '	$(CXX) $(CFLAGS) $(CPP_OBJS) $(ENGINE_FLAGS) -o $(NAME)$(NAME_X)'
+  ''
+  buildraylib:
+  '	make -C ../Engine/include/raylib/src/'
+  ''
+  clean:
+  '	@rm -fv $(CPP_OBJS)'
+  ''
+  'fclean: clean'
+  '	@rm -fv ../$(NAME)$(NAME_X)'
+  ''
+  're: fclean all'
+  ''
+  '.PHONY: Engine'
+)
+
+i_compileRule=${#compileRule[@]}
+j_compileRule=0
+
+function printCompileRule() {
+  echo -n
+  while [ $j_compileRule -lt $i_compileRule ]
+  do
+    echo "${compileRule[$j_compileRule]}" >> $1
+    j_compileRule=$((j_compileRule + 1))
+  done
+}
+
+################################
+#
+#          Linux
+#
+################################
+
+linuxHeader=(
+  ''
+  ''
+  '# WINDOS MAKEFILE '
+  'NAME			=			IceFactory'
+  'NAME_X		=			.exe'
+  ''
+  '# - - - -'
+  ''
+  "RAYLIB		=	$raylibPath"
+  'ENGINE_FLAGS		=		$(RAYLIB) -lgdi32 -lwinmm'
+  'ENGINE_SETTING	=		-D 'HOT_RELOAD=1' -Wno-missing-field-initializers'
+  ''
+  'DEBUG						=		-g'
+  ''
+  ''
+)
+
+i_winHeader=${#winHeader[@]}
+j_winHeader=0
+
+linuxFilename=Makefile
 
 function makelinux() {
   rm -fv $linuxFilename
   echo 'touch' $linuxFilename
   touch $linuxFilename
-  while [ $j -lt $i ]
+  printSysHeader $linuxFilenamev
+  while [ $j_winHeader -lt $i_winHeader ]
   do
-    echo "${varAndFiles[$j]}" >> $linuxFilename
-    j=$((j + 1))
+    echo "${winHeader[$j_winHeader]}" >> $WindowsFilename
+    j_winHeader=$((j_winHeader + 1))
   done
+  drawCompiler $linuxFilename
+  printFiles $linuxFilename
+  printGameFiles $linuxFilename
+  printCompileRule $linuxFilename
 }
 
-WindowsFilename=makeFileWindows
+
+################################
+#
+#          Windows
+#
+################################
+
+winHeader=(
+  ''
+  ''
+  '# WINDOS MAKEFILE '
+  'NAME			=			IceFactory'
+  'NAME_X		=			.exe'
+  ''
+  '# - - - -'
+  ''
+  "RAYLIB		=	$raylibPath"
+  'ENGINE_FLAGS		=		$(RAYLIB) -lgdi32 -lwinmm'
+  'ENGINE_SETTING	=		-D 'HOT_RELOAD=1' -Wno-missing-field-initializers'
+  ''
+  'DEBUG						=		-g'
+  ''
+  ''
+)
 
 i_winHeader=${#winHeader[@]}
 j_winHeader=0
+
+WindowsFilename=test/Makefile
 
 function makeWindos() {
   rm -fv $WindowsFilename
@@ -100,6 +320,12 @@ function makeWindos() {
     echo "${winHeader[$j_winHeader]}" >> $WindowsFilename
     j_winHeader=$((j_winHeader + 1))
   done
+  drawCompiler $WindowsFilename
+  printFiles $WindowsFilename
+  printGameFiles $WindowsFilename
+  printCompileRule $WindowsFilename
 }
+
+
 
 makeWindos
